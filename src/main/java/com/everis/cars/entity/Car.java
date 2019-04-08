a0beef66dad1295b5835e193f67741d51e042d97
jbarbaso <jbarbaso@everis.com>
2019-04-08 12:19:01 +0200

    Updated Car Entity with all the fields required, annotations and some validations.

--- src/main/java/com/everis/cars/entity/Car.java
+++ src/main/java/com/everis/cars/entity/Car.java
@@ -5,24 +5,40 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name="car")
+@NamedQuery(name = Car.FIND_ALL, query = "select c from Car c")
 public class Car {
+	
+	public static final String FIND_ALL = "Car.findAll";
+	
 	@Id
-	protected String id;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	protected Number id;
 
 	@Column
+	@NotNull
+	@Size(min=2, max=50)
 	protected String brand;
 
 	@Column
+	@NotNull
 	protected Timestamp registration;
 
 	@Column
+	@NotNull
+	@Size(min=2, max=100)
 	protected String country;
 
 	@Column
@@ -41,11 +57,11 @@ public class Car {
 		updated_at = (Timestamp) new Date();
 	}
 	
-	public String getId() {
+	public Number getId() {
 		return id;
 	}
 
-	public void setId(String id) {
+	public void setId(Number id) {
 		this.id = id;
 	}
 
