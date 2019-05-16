30651bcddcda613d29b070759defbe697ef1e150
evJbarbaso <jbarbaso.everis@gmail.com>
2019-05-16 08:47:29 +0200

    Added more tests and comments to CarMessageListenerTest

--- src/test/java/com/everis/cars/jms/CarMessageListenerTest.java
+++ src/test/java/com/everis/cars/jms/CarMessageListenerTest.java
@@ -3,6 +3,7 @@ package com.everis.cars.jms;
 import static com.everis.cars.utils.TestUtils.mockCar;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -22,7 +23,6 @@ import javax.json.bind.JsonbConfig;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -36,7 +36,8 @@ import org.powermock.modules.junit4.rule.PowerMockRule;
 import com.everis.cars.control.CarService;
 import com.everis.cars.entity.Car;
 import com.everis.cars.exceptions.CarNotFoundException;
-import com.everis.cars.exceptions.InvalidActionException;
+import com.everis.cars.exceptions.InvalidActionMessageException;
+import com.everis.cars.exceptions.InvalidCarMessageException;
 import com.everis.cars.utils.ValidatorUtil;
 
 /**
@@ -48,12 +49,16 @@ import com.everis.cars.utils.ValidatorUtil;
 public class CarMessageListenerTest {
 
 	/**
-	 * We use the {@link Rule} annotation instead of {@link RunWith} annotation to
-	 * indicate the mockito runner
+	 * We use the {@link PowerMockRule} with {@link Rule} annotation to indicate the
+	 * mockito runner
 	 */
 	@Rule
 	public PowerMockRule rule = new PowerMockRule();
 
+	/**
+	 * We use the {@link MockitoRule} with {@link Rule} annotation to indicate the
+	 * powermock runner
+	 */
 	@Rule
 	public MockitoRule mockitoRule = MockitoJUnit.rule();
 
@@ -79,84 +84,117 @@ public class CarMessageListenerTest {
 	 */
 	private Jsonb jsonb;
 
+	/**
+	 * Mock a {@link Car} instance to be used in different tests
+	 */
 	private Car car;
+
+	/**
+	 * Mock a {@link Car} instance in json format to be used in different tests
+	 */
 	private String jsonCar;
+
+	/**
+	 * Mock a {@link Message} instance to be used in different tests
+	 */
 	private Message message;
 
+	/**
+	 * Initialize some utils and mocks to be used for different tests
+	 */
 	@Before
 	public void setUp() {
-		// mock JsonbConfig creation
+		// mock Jsonb with custom config
 		jsonbConfig = new JsonbConfig().withFormatting(true);
-		// mock Jsonb creation by JsonbBuilder.create static method
 		jsonb = JsonbBuilder.create(jsonbConfig);
 
-		// mock car object
-		car = mockCar("BMW", LocalDateTime.now(), "Spain");
-		// mock jsonCar
-		jsonCar = jsonb.toJson(car, Car.class);
-		// mock message with extraInterfaces method to set allow cast it to TextMessage
-		message = mock(Message.class, withSettings().extraInterfaces(TextMessage.class));
-
-		// mockStatic class JsonBuilder to intercept the create method returning a valid
-		// Jsonb object
+		// mockStatic class JsonBuilder class
 		PowerMockito.mockStatic(JsonbBuilder.class);
+
 		// mock Jsonb creation by JsonbBuilder.create static method
 		when(JsonbBuilder.create(jsonbConfig)).thenReturn(jsonb);
 
-		// mockStatic class ValidatorUtil to intercept the valid method in test
+		// mock car and jsonCar objects
+		car = mockCar("BMW", LocalDateTime.now(), "Spain");
+		jsonCar = jsonb.toJson(car, Car.class);
+
+		// mock message with extraInterfaces method to allow cast it to TextMessage
+		message = mock(Message.class, withSettings().extraInterfaces(TextMessage.class));
+
+		// mockStatic class ValidatorUtil
 		PowerMockito.mockStatic(ValidatorUtil.class);
 	}
 
 	/**
-	 * Mocking JMS connection, session and producer to send a message to the queue
-	 * with a valid {@link Car} object and the action to be executed
+	 * Consume a message from the queue with a valid {@link Car} object and the
+	 * action to be executed. Will execute onMessage method and execute the action
+	 * given correctly.
 	 * 
 	 * @return void
-	 * @throws JMSException
-	 * @throws InvalidActionException
-	 * @throws CarNotFoundException
+	 * @throws JMSException                  if there is a jms error
+	 * @throws InvalidActionMessageException if there is an invalid action in given
+	 *                                       message
+	 * @throws CarNotFoundException          if there is not car in database if
+	 *                                       action requires update or delete
 	 */
 	@Test
 	public void ConsumeMessage_ValidMessage_ExecuteActionCorrectly()
-			throws JMSException, CarNotFoundException, InvalidActionException {
+			throws JMSException, CarNotFoundException, InvalidActionMessageException {
 		// mock message.getObjectProperty method return action String
 		when(message.getObjectProperty(Mockito.anyString())).thenReturn("POST");
 
 		// mock message.getText
 		when(((TextMessage) message).getText()).thenReturn(jsonCar);
-
 		when(ValidatorUtil.validate(car)).thenReturn(new ArrayList<String>());
 
+		// when execute onMessage
 		carMessageListener.onMessage(message);
 
+		// then verify methods execution
 		verify(message, times(1)).getObjectProperty("action");
 		verify((TextMessage) message, times(1)).getText();
 		verify(carService, times(1)).createCar(Mockito.any());
 	}
 
+	/**
+	 * Consume a message from the queue with a valid {@link Car} object but invalid
+	 * action value. On executeAction will throw an
+	 * {@link InvalidActionMessageException}.
+	 */
 	@Test
 	public void ConsumeMessage_InvalidActionProperty_ThrowsInvalidActionException() {
 		try {
+			// given car object form setUp method and invalid action
+			// when execute executeAction method
 			carMessageListener.executeAction(car, "ANY");
-		} catch (CarNotFoundException | InvalidActionException e) {
-			assertTrue(e instanceof InvalidActionException);
+		} catch (CarNotFoundException | InvalidActionMessageException e) {
+			// then this throws an InvalidActionMessageException
+			assertTrue(e instanceof InvalidActionMessageException);
 			assertEquals(e.getMessage(), "Invalid action ANY specified.");
 		}
 	}
 
+	/**
+	 * Consume a message from the queue with invalid {@link Car} value. On
+	 * isValidCarMessage method execution it will throw an
+	 * {@link InvalidCarMessageException}.
+	 */
 	@Test
 	public void ConsumeMessage_InvalidCar_ThrowsConstraintViolation() {
-		// mock car object
+		// given invalid car and errors from ValidatorUtil
 		final Car invalidCar = mockCar("", LocalDateTime.now(), "Spain");
 		ArrayList<String> errors = new ArrayList<String>();
-		errors.add("one error");
+		errors.add("Field brand can't be null");
 
-		when(ValidatorUtil.validate(car)).thenReturn(errors);
+		// when validate return errors
+		when(ValidatorUtil.validate(any())).thenReturn(errors);
 		try {
-			carMessageListener.executeAction(invalidCar, "POST");
-		} catch (CarNotFoundException | InvalidActionException e) {
-//			assertTrue(e instanceof InvalidActionException);
-//			assertEquals(e.getMessage(), "Invalid action ANY specified.");
+			// when execute isValidCarMessage method
+			carMessageListener.isValidCarMessage(invalidCar);
+		} catch (InvalidCarMessageException e) {
+			// then this throws an InvalidCarMessageException
+			assertTrue(e instanceof InvalidCarMessageException);
+			assertEquals(e.getMessage(), "Message has invalid car object. \nValidation errors: \n" + errors);
 		}
 	}
 
